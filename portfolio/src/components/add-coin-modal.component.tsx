14456dd7db397d8185c0faeb04bac57e5f934044
Ivan <ivanmacevichwrk@gmail.com>
2023-12-27 12:11:42 +0300

    feat: setup project

---
+++ portfolio/src/components/add-coin-modal.component.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddCoinModal = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AddCoinModal;
---
+++ portfolio/src/pages/crypto-info.page.tsx
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+const CryptoInfo = () => {
+    const [activities, setActivities] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const url = "/api/1/brc20/balance?limit=20&offset=0&address=bc1qhgmu47f3kdqkqd03pctx89dt0rcyscchdxqlnm";
+            const headers = {
+                "accept": "application/json",
+                "api-key": "142cf1b0-1ca7-11ee-bb5e-9d74c2e854ac",
+            };
+
+            try {
+                const response = await axios.get(url, { headers, });
+                const data = response.data;
+                setActivities(data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+    return (
+        <div>
+            {activities !== null ? (
+                <pre>{JSON.stringify(activities, null, 2)}</pre>
+            ) : (
+                <p>Loading...</p>
+            )}
+        </div>
+    );
+};
+
+export default CryptoInfo;
