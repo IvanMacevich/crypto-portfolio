8f41c86c6c3c33f700954d4f9019bfeba66728df
DmitryVaneka <dmitry.vaneka@interexy.com>
2024-01-04 23:19:29 +0300

    fix

--- portfolio/src/app/portfolio/store/coin-info/coin-info.selectors.ts
+++ portfolio/src/app/portfolio/store/coin-info/coin-info.selectors.ts
@@ -1,9 +1,6 @@
 import { RootState } from "../../../../store/store";
 
 export const selectCoinInfo = (state: RootState) => state.coinInfo.data.list;
-<<<<<<< HEAD
 export const selectBtcPrice = (state: RootState) =>
   state.coinInfo.data.BTCPrice;
 export const selectCoinStatus = (state: RootState) => state.coinInfo.status;
-=======
->>>>>>> 6ff3d02fcf3a19e5c0d2cb9446f7e3adca26fec2
